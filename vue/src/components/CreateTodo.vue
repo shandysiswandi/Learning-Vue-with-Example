8f483a3d9562a066fe7f82062bab4412b5da2da5
shandysiswandi <shandysiswandi@gmail.com>
2020-06-14 16:17:05 +0700

    add createtodo component

---
+++ vue/src/components/CreateTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <form class="col-12 bg-dark p-3" @submit.prevent="addTodo()">
+    <input
+      v-model="newTodo"
+      type="text"
+      class="form-control"
+      placeholder="Add todo here... Enter to save"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "CreateTodo",
+  data() {
+    return {
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.length > 0) {
+        this.$emit("onCreate", this.newTodo);
+      }
+      this.newTodo = "";
+    }
+  }
+};
+</script>
+
