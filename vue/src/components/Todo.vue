47a8fc7cca42d6a367d60c345b2a65669b271d9d
shandysiswandi <shandysiswandi@gmail.com>
2020-06-14 16:17:14 +0700

    add todo component

---
+++ vue/src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card text-white mb-3" :class="{'bg-primary':completed, 'bg-danger':!completed}">
+    <div class="card-body" @click="$emit('on-done')">
+      <p class="card-text" :class="{completed}">
+        {{title}}
+        <span>[{{id}}]</span>
+      </p>
+    </div>
+    <div class="card-footer bg-white">
+      <div class="btn-group btn-block" role="group" aria-label="Basic example">
+        <button @click="$emit('on-delete')" type="button" class="btn btn-sm btn-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: {
+    id: Number,
+    title: String,
+    completed: Boolean
+  }
+};
+</script>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
