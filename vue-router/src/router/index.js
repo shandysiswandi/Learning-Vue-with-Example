07c3dd7269b2ab6f51b29d427460cd56fe12e0ef
shandysiswandi <shandysiswandi@gmail.com>
2020-06-15 17:35:06 +0700

    implement view component

--- vue-router/src/router/index.js
+++ vue-router/src/router/index.js
@@ -1,29 +1,15 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { routes } from "./view.js";
 
-Vue.use(VueRouter)
-
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+  linkActiveClass: "active", // active class for non-exact links.
+  linkExactActiveClass: "active", // active class for *exact* links.
+});
 
-export default router
+export default router;
