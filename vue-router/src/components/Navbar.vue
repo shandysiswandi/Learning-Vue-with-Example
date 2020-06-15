1f8a003afa685b72985c16f7d6314042e0ce5d70
shandysiswandi <shandysiswandi@gmail.com>
2020-06-15 17:34:27 +0700

    add navbar

---
+++ vue-router/src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-3">
+    <div class="container">
+      <router-link to="/" class="navbar-brand mb-0 h1">Vue Router</router-link>
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <router-link to="/" exact class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/services" class="nav-link">Services</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/skills" class="nav-link">Skills</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/education" class="nav-link">Education</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/experience" class="nav-link">Experience</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/work" class="nav-link">Work</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/blog" class="nav-link">Blog</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
